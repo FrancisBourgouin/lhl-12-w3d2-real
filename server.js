036a4019f429361fd303a3e2f240a2788213a7cf
Dominic Tremblay <dominictremblay24@gmail.com>
2019-07-08 21:01:50 -0400

    add logpayload middleware

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+const logPayload = (req, res, next) => {
+  console.log('Request parameters:');
+  console.log('-'.repeat(30));
+  console.log('Method:', req.method);
+  console.log('Url:', req.url);
+  console.log('req.params:', req.params);
+  console.log('req.body:', req.body);
+  console.log('-'.repeat(30));
+  next();
+};
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(logPayload);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
